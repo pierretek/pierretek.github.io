e8ec29b40d128e7423352560801278eac7aee6d0
pierretek <pierretekdev@gmail.com>
2026-03-14 17:53:55 -0400

    refactor button styles and update about me

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@
 //funny demo thing
 document.getElementById("funny-demo").addEventListener("click", function () {
 	const original = this.textContent;
-	this.textContent = "You're already here dummy!";
+	this.textContent = "You're already here dummy";
 
 	setTimeout(() => {
 		this.textContent = original; 
